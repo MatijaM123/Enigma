8d5c69191c74dfbcd2bb9fa43fcab98b041b6e18
Marence1234 <2021.09.05@online.devetagimnazija.edu.rs>
2024-05-24 17:17:49 +0200

    Dodata plugboard klasa, u zaseban fajl. Sadrzi niz charova 'Izlazna', koja prikazuje izlazna slova na plugboardu. Ako slovo nije povezano, prikazuje se '.' . Sadrzi metodu public void KlikSlovo(char s) koja ce se aktivirati kada se poveze (klikne) neko slovo na plugboardu, bilo ono prvo ili drugo po redu. Metoda sadrzi sve provere i radi bezuslovno. Metoda private void SpojiSlova(char s1, char s2) koja belezi koja slova na plug boardu su spojena, tj. menja niz Izlazna.

---
+++ Enigma/Plugbord.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+public class Plugboard : IUlazIzlaz
+{
+    public char[] Izlazna { get; private set; }
+    char kliknuto; // slovo koje je kliknuto
+    public Plugboard()
+    {
+        Izlazna = new char[] { '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.' };
+        kliknuto = '-';
+    }
+    public void KlikSlovo(char s)
+    {
+        //provera da li je vec povezano
+        int i1 = s - 97;
+        if (Izlazna[i1] != '.') // vec je povezan
+        {
+            int i2 = Izlazna[i1] - 97;// index drugog slova u paru
+            Izlazna[i2] = '.';
+            Izlazna[i1] = '.';
+        }
+        //provera da li je kliknuto drugo slovo u paru
+        if (kliknuto == '-')
+            kliknuto = s;
+        else if (kliknuto == s)
+            kliknuto = '-';
+        else
+            SpojiSlova(kliknuto, s);
+    }
+    private void SpojiSlova(char s1, char s2)
+    {
+        int i1 = s1 - 97;
+        int i2 = s2 - 97;
+        Izlazna[i1] = s2;
+        Izlazna[i2] = s1;
+        kliknuto = '-';
+    }
+}
