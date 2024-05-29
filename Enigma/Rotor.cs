a7849e3c876dc751dc59f311d180f3beaae2fe9b
Matija Marinkovic <bombmatija@gmail.com>
2024-05-29 14:53:11 +0200

    Dodat interfejs ISifrovanje i klasa Rotor

---
+++ Enigma/Rotor.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Enigma
+{
+    internal class Rotor : ISifrovanje
+    {
+        List<char> elementi = new List<char>();
+        public char Sifruj(char x, bool SuprotniSmer = false) => SuprotniSmer?(char)('A'+elementi.IndexOf(x)):elementi[x - 'A'];
+    }
+}
