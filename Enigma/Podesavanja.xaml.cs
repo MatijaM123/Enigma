fac4788ca05d7037a90f0ab40118cd8e5be0ee2b
aleksavukovicc <113255172+aleksavukovicc@users.noreply.github.com>
2024-06-02 14:58:35 +0200

    prebacivanje slova iz podesavanja na rotore

--- Enigma/Podesavanja.xaml.cs
+++ Enigma/Podesavanja.xaml.cs
@@ -35,7 +35,34 @@ namespace Enigma
             Main.R1S.Content = cbx_pozicija1.Text;
             Main.R2S.Content = cbx_pozicija2.Text;
             Main.R3S.Content = cbx_pozicija3.Text;
+            Main.R1G.Content = SledeceSlovo(cbx_pozicija1.Text);
+            Main.R2G.Content = SledeceSlovo(cbx_pozicija2.Text);
+            Main.R3G.Content = SledeceSlovo(cbx_pozicija3.Text);
+            Main.R1D.Content = ProsloSlovo(cbx_pozicija1.Text);
+            Main.R2D.Content = ProsloSlovo(cbx_pozicija2.Text);
+            Main.R3D.Content = ProsloSlovo(cbx_pozicija3.Text);
             Close();
+        }
+
+        private string SledeceSlovo(string slovo)
+        {
+            char ch = (char)(slovo[0]+1);
+            if (ch == 'Z'+1)
+            {
+                return "A";
+            }
+            return ch.ToString();
+
+
+        }
+        private string ProsloSlovo(string slovo)
+        {
+            char ch = (char)(slovo[0] - 1);
+            if (ch == 'A' - 1)
+            {
+                return "Z";
+            }
+            return ch.ToString();
         }
     }
 }
