9106376e655af632174be5b44de6908e87e79691
Katarina <2021.09.19@online.devetagimnazija.edu.rs>
2024-06-02 17:28:47 +0200

    Napravljen konstruktor za reflektor

--- Enigma/Reflektor.cs
+++ Enigma/Reflektor.cs
@@ -11,5 +11,71 @@ namespace Enigma
 
         List<char> elementi = new List<char>();
         public char Sifruj(char x, bool smer = false) => elementi[x - 'A'];
+        public Reflektor(int redBroj)
+        {
+            if (redBroj < 0 || redBroj > 1)
+                throw new Exception($"Ne postoji rotor {redBroj}");
+            else
+            {
+                if(redBroj == 0)
+                {
+                    elementi.Add('Y');
+                    elementi.Add('R');
+                    elementi.Add('U');
+                    elementi.Add('H');
+                    elementi.Add('Q');
+                    elementi.Add('S');
+                    elementi.Add('L');
+                    elementi.Add('D');
+                    elementi.Add('P');
+                    elementi.Add('X');
+                    elementi.Add('N');
+                    elementi.Add('G');
+                    elementi.Add('O');
+                    elementi.Add('K');
+                    elementi.Add('M');
+                    elementi.Add('I');
+                    elementi.Add('E');
+                    elementi.Add('B');
+                    elementi.Add('F');
+                    elementi.Add('Z');
+                    elementi.Add('C');
+                    elementi.Add('W');
+                    elementi.Add('V');
+                    elementi.Add('J');
+                    elementi.Add('A');
+                    elementi.Add('T');
+                }
+                else
+                {
+                    elementi.Add('F');
+                    elementi.Add('V');
+                    elementi.Add('P');
+                    elementi.Add('J');
+                    elementi.Add('I');
+                    elementi.Add('A');
+                    elementi.Add('O');
+                    elementi.Add('Y');
+                    elementi.Add('E');
+                    elementi.Add('D');
+                    elementi.Add('R');
+                    elementi.Add('Z');
+                    elementi.Add('X');
+                    elementi.Add('W');
+                    elementi.Add('G');
+                    elementi.Add('C');
+                    elementi.Add('T');
+                    elementi.Add('K');
+                    elementi.Add('U');
+                    elementi.Add('Q');
+                    elementi.Add('S');
+                    elementi.Add('B');
+                    elementi.Add('N');
+                    elementi.Add('M');
+                    elementi.Add('H');
+                    elementi.Add('L');
+                }
+            }
+        }
     }
 }
